16aab330eb06f0220ebe9670c48204adb2551e73
1arthurka1 <kingsprotect@gmail.com>
2023-05-02 11:34:13 +0200

    02.05

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const imagesToPaste = galleryItems.map(item => {
+    const { preview, original, description } = item;
+
+  return  `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+          loading="lazy"
+        />
+      </a>
+    </li>`
+      
+}
+)
+galleryRef.insertAdjacentHTML("beforeend", imagesToPaste.join(''))
+const option = {
+  captionsData: 'alt',
+captionDelay: 250,
+}
+
+const bigImg = new SimpleLightbox('.gallery a', option);
